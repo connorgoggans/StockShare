5be6e4f31c2d81f884590dee8440266aff6bf2c8
Irfaan Khalid <irfaan.khalid@vanderbilt.edu>
2017-03-25 14:08:46 -0500

    Added example config file

---
+++ server/src/config_example.js
@@ -0,0 +1,10 @@
+// Inspired by the trvl repo
+
+var config = {};
+
+// Firebase
+config.firebase = {};
+config.firebase.apiKey = "API KEY";
+config.firebase.databaseURL = "YOUR URL";
+
+module.exports = config;
