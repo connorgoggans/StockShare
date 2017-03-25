ad86332338a4ed95a6f8265c097f43a623107399
Irfaan Khalid <irfaan.khalid@vanderbilt.edu>
2017-03-25 14:42:01 -0500

    Moved to database branch. Also added database.js file.

---
+++ server/src/database.js
@@ -0,0 +1,15 @@
+var firebase = require('firebase');
+
+function initializeFirebase() {
+    firebase.initializeApp(config.firebase);
+    var database = firebase.database();
+    var ref = database.ref('vals');
+
+    ref.on('value', function(data) {
+        users = data.val();
+        //   console.log(users);
+
+    }, errData);
+}
+// Initialize Firebase
+initializeFirebase();
