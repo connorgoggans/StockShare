cb9e037d421b3d9a949ad5c73570cdc9d2d9efc5
Benjamin Cooper <bencooper222@gmail.com>
2017-03-25 23:24:03 -0500

    Updated config_example with Braintree stuff

--- server/src/config_example.js
+++ server/src/config_example.js
@@ -7,6 +7,13 @@ config.firebase = {};
 config.firebase.apiKey = "API KEY";
 config.firebase.databaseURL = "YOUR URL";
 
+// Braintree    
+config.braintree = {};
+config.braintree.merchantId = "MERCHANT ID";
+config.braintree.publicKey = "PUBLIC KEY";
+config.braintree.privateKey = "PRIVATE KEY";
+
+
 
 //Ports
 config.ports = {};
