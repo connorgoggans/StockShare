a254a2449ea1c9d2b2040d7bde598e5fa0cb64ed
Benjamin Cooper <bencooper222@gmail.com>
2017-03-25 19:09:04 -0500

    Broke all branch conventions but separated express stuff from braintree stuff

--- server/src/braintree.js
+++ server/src/braintree.js
@@ -1,19 +1,6 @@
 var braintree = require('braintree');
 var config = require("./config.js");
-var express = require('express')
-var bodyParser = require('body-parser');
-var cors = require('cors');
 
-var app = express();
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 
 
@@ -24,7 +11,11 @@ var gateway = braintree.connect({
     privateKey: config.braintree.privateKey
 });
 
-
+function generateToken(res) {
+    gateway.clientToken.generate({}, function(err, response) {
+        res.send(response.clientToken);
+    });
+}
 
 function executeTransaction(paymentNonce, amount, displayWebpage) {
     gateway.transaction.sale({
@@ -40,28 +31,6 @@ function executeTransaction(paymentNonce, amount, displayWebpage) {
 }
 
 
-// gives the token to the client to individually authorize the payment
-app.get("/client_token", function(req, res) {
-    gateway.clientToken.generate({}, function(err, response) {
-        res.send(response.clientToken);
-    });
-});
 
-app.get('/', function(req, res) {
-    res.send('Hello World!')
-});
-
-app.post("/checkout", function(req, res) {
-    console.log("hey");
-    console.log(req.body);
-
-    console.log(nonceFromTheClient);
-    // Use payment method nonce here
-    executeTransaction(req.body.payment_method_nonce, req.body.amount, function(page) {
-        res.send(page);
-    });
-});
-
-app.listen(config.ports.listen, function() {
-    console.log('Example app listening on port 3000!')
-})
+exports.transaction = executeTransaction;
+exports.token = generateToken;
