6d5c7cd191e1a5630d668fa18cf9cf19a56cace4
Connor Goggans <connor.goggans@vanderbilt.edu>
2017-03-25 21:42:37 -0500

    fixed robinhood.js and added stock info

--- server/src/robinhood.js
+++ server/src/robinhood.js
@@ -22,7 +22,7 @@ function buyAction(symbol)(){
                   }
               }
 
-              Robinhood.place_sell_order(options, function(error, response, body){
+              Robinhood.place_buy_order(options, function(error, response, body){
                   if(error){
                       console.error(error);
                   }else{
@@ -56,7 +56,7 @@ function sellAction(symbol)(){
                   }
               }
 
-              Robinhood.place_buy_order(options, function(error, response, body){
+              Robinhood.place_sell_order(options, function(error, response, body){
                   if(error){
                       console.error(error);
                   }else{
@@ -69,5 +69,32 @@ function sellAction(symbol)(){
   });
 }
 
+function getStockInfo(symbol(){
+  var Robinhood = require('robinhood')(credentials, function(){
+    Robinhood.fundamentals(symbol, function(error, response, body){
+        if(error){
+            console.error(error);
+        }else{
+            console.log(body);
+            //{                               // Example for SBPH
+            //    average_volume: string,     // "14381.0215"
+            //    description: string,        // "Spring Bank Pharmaceuticals, Inc. [...]"
+            //    dividend_yield: string,     // "0.0000"
+            //    high: string,               // "12.5300"
+            //    high_52_weeks: string,      // "13.2500"
+            //    instrument: string,         // "https://api.robinhood.com/instruments/42e07e3a-ca7a-4abc-8c23-de49cb657c62/"
+            //    low: string,                // "11.8000"
+            //    low_52_weeks: string,       // "7.6160"
+            //    market_cap: string,         // "94799500.0000"
+            //    open: string,               // "12.5300"
+            //    pe_ratio: string,           // null (price/earnings ratio)
+            //    volume: string              // "4119.0000"
+            //}
+        }
+    })
+});
+})
+
 exports.buy = buyAction;
 exports.sell = sellAction;
+exports.info = getStockInfo;
