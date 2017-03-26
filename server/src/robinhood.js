19340b0870391ed77d8ab6adb4fc3955346a816b
Connor Goggans <connor.goggans@vanderbilt.edu>
2017-03-25 19:35:42 -0500

    can buy/sell on robinhood

---
+++ server/src/robinhood.js
@@ -0,0 +1,69 @@
+var credentials = require('masterAccountCredentials.js')();
+
+function buyAction(symbol)(){
+  var Robinhood = require('robinhood')(credentials, function(){
+      Robinhood.instruments(symbol,function(err, response, body){
+          if(err){
+              console.error(err);
+          }else{
+              console.log(body);
+              var url = body.results[0].url;
+              var stockSymbol = body.results[0].symbol;
+              console.log(url);
+
+              var options = {
+                  type: 'limit',
+                  quantity: 1,
+                  bid_price: 1.00,
+                  instrument: {
+                    url: url,
+                    symbol: stockSymbol
+                  }
+              }
+
+              Robinhood.place_sell_order(options, function(error, response, body){
+                  if(error){
+                      console.error(error);
+                  }else{
+                      console.log(body);
+                  }
+              })
+          }
+      })
+
+  });
+}
+
+function sellAction(symbol)(){
+  var Robinhood = require('robinhood')(credentials, function(){
+      Robinhood.instruments(symbol,function(err, response, body){
+          if(err){
+              console.error(err);
+          }else{
+              console.log(body);
+              var url = body.results[0].url;
+              var stockSymbol = body.results[0].symbol;
+              console.log(url);
+
+              var options = {
+                  type: 'limit',
+                  quantity: 1,
+                  bid_price: 1.00,
+                  instrument: {
+                    url: url,
+                    symbol: stockSymbol
+                  }
+              }
+
+              Robinhood.place_buy_order(options, function(error, response, body){
+                  if(error){
+                      console.error(error);
+                  }else{
+                      console.log(body);
+                  }
+              })
+          }
+      })
+
+  });
+}
